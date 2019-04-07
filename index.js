c422c39dcc9c2872216a5d043f23fc22708d402c
Oscar Korz <okorz001@gmail.com>
2019-04-07 13:00:07 -0700

    Get user

--- index.js
+++ index.js
@@ -20,6 +20,33 @@ class DuolingoClient {
         this.jwt = null
     }
 
+    async getUser(username) {
+        // this URL returns more data when you are logged in as the same user
+        const res = await http.get(`https://www.duolingo.com/users/${username}`)
+        const languages = res.body.languages
+            // ignore languages that aren't being learned
+            .filter(it => it.learning)
+            .map(it => ({
+                id: it.language,
+                name: it.language_string,
+                level: it.level,
+                points: it.points,
+            }))
+            // order by points descending
+            .sort((a, b) => b.points - a.points)
+        return {
+            id: res.body.id,
+            username: res.body.username,
+            fullname: res.body.fullname,
+            streak: {
+                length: res.body.site_streak,
+                extended: res.body.streak_extended_today,
+                freeze: !!res.body.inventory.streak_freeze,
+            },
+            languages,
+            activeLanguage: res.body.learning_language,
+        }
+    }
 }
 
 module.exports = {
